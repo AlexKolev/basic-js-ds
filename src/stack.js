789f309a01e9418dd5b360cb5e2d99c21a982724
Defry <defryalex@gmail.com>
2022-04-24 20:08:52 +0500

    feat: make stack

--- src/stack.js
+++ src/stack.js
@@ -12,23 +12,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+class StackElement {
+	constructor(x) {
+		this.value = x;
+		//this.next = null;
+		this.prev = null;
+	}
+}
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.currentElement = null;
+	}
+	push(element) {
+		//throw new NotImplementedError('Not implemented');
+		// remove line with error and write your code here
+		if (!this.currentElement) {
+			this.currentElement = new StackElement(element);
+		} else {
+			let newElement = new StackElement(element);
+			newElement.prev = this.currentElement;
+			this.currentElement = newElement;
+		}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	pop() {
+		//throw new NotImplementedError('Not implemented');
+		// remove line with error and write your code here
+		if (!this.currentElement) {
+			return undefined;
+		}
+		let retElement = this.currentElement;
+		if (this.currentElement.prev) {
+			this.currentElement = this.currentElement.prev;
+		}
+		return retElement.value;
+	}
+
+	peek() {
+		return this.currentElement.value;
+	}
 }
 
 module.exports = {
-  Stack
+	Stack
 };
+
+
+const stack = new Stack();
+stack.push(5);
+stack.push(6);
+stack.push(7);
+console.log(stack.peek());//, 7);
+console.log(stack.pop());//, 7);
+console.log(stack.peek());//, 6);
+
+
+
+// const stack = new Stack();
+
+// stack.push(1); // adds the element to the stack
+// console.log(stack.peek()); // returns the peek, but doesn't delete it, returns 1
+// //stack.pop(); // returns the top element from stack and deletes it, returns 1
+// console.log(stack.pop());
+// console.log(stack.pop());
+// //stack.pop(); // undefined
